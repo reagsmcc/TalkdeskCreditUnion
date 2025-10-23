1fe6ef67c098eeef22afac7663d00f7257a31aa0
reagsmcc <reagan.mccauley@talkdesk.com>
2025-10-23 09:37:03 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+ * Talkdesk Credit Union Fictitious Website JavaScript
+ *
+ * Add any interactive functionality here!
+ * Examples:
+ * - Mobile navigation toggle
+ * - Form validation
+ * - Simple interactive calculators
+ */
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Basic example: console log when the page loads
+    console.log("Talkdesk Credit Union Website Loaded!");
+});
